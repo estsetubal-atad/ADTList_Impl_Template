360e1d2f3693e322693b1553f4bbdbf31fa6408e
brunomnsilva <brunomnsilva@gmail.com>
2023-02-22 16:43:57 +0000

    project structure update

--- mem_check.sh
+++ mem_check.sh
@@ -1,5 +1,6 @@
 !#/bin/sh
 
-# you must compile the program beforehand with debug symbols 
+# You must compile the program beforehand with debug symbols:
+# $> make debug 
 
 valgrind --leak-check=full --show-reachable=yes --track-origins=yes ./prog
